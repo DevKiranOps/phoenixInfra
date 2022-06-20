62ad38cd79b3fd2be23801a47dbddb1130fd7f80
Kiran <kiran@yahoo.com>
2022-06-20 08:55:35 +0530

    variabilized subnet cidrs

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "myvpc" {
 
   tags = {
     Name        = "${var.prefix}-vpc"
-    Environment =  var.env
+    Environment = var.env
     Project     = var.project
   }
 }
@@ -14,39 +14,38 @@ resource "aws_vpc" "myvpc" {
 # Public Subnet 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.100.1.0/24"
+  cidr_block = var.pubSubnetCidr
 
   tags = {
     Name        = "${var.prefix}-pubsubnet"
-    Environment =  var.env
-    Project     =  var.project
+    Environment = var.env
+    Project     = var.project
   }
 }
 
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.100.5.0/24"
+  cidr_block = var.privateSubnetCidr
 
   tags = {
     Name        = "${var.prefix}-prvsubnet"
-    Environment =  var.env
+    Environment = var.env
     Project     = var.project
   }
 
 }
 
 resource "aws_subnet" "web" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.100.3.0/24"
-  depends_on = [aws_subnet.public] 
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.webSubnetCidr
+  map_public_ip_on_launch = true
   tags = {
     Name        = "${var.prefix}-websubnet"
-    Environment =  var.env
+    Environment = var.env
     Project     = var.project
   }
 }
-
 
 
 resource "aws_internet_gateway" "mygw" {
@@ -54,10 +53,36 @@ resource "aws_internet_gateway" "mygw" {
 
   tags = {
     Name        = "${var.prefix}-igw"
-    Environment =  var.env
-    Project     =  var.project
+    Environment = var.env
+    Project     = var.project
   }
 }
 
 
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    Name        = "${var.prefix}-igw"
+    Environment = var.env
+    Project     = var.project
+  }
+}
+
+resource "aws_route" "igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mygw.id
+
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "web" {
+  subnet_id      = aws_subnet.web.id
+  route_table_id = aws_route_table.public.id
+}
